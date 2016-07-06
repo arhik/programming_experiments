07bab265e43a5bf86031e1db7743fb8f92b12dba
arhik <karthik.katipalli@gmail.com>
2016-07-05 22:54:41 -0400

    GPL Head

--- Python/gradient descent.py
+++ Python/gradient descent.py
@@ -1,9 +1,22 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 25 07:01:50 2016
 
-@author: arhik
-"""
+# gradient descent.py
+#
+# Copyright (C) 2016 arhik <karthik.katipalli@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 
