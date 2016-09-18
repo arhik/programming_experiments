515b0ab5c77aebff2eeda5257ee1e99784e64d81
arhik <karthik.katipalli@gmail.com>
2016-09-17 21:16:22 -0400

    Documenting the modules

--- Python/interview/intset.py
+++ Python/interview/intset.py
@@ -1,33 +1,101 @@
+#!/usr/bin/env python2
+
+# Copyright (C) 2016  <arhik@groundsignal.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+"""
+This module defines the IntSet datastructure and
+an is_subset function.
+"""
+
 class IntSet:
+    """
+    IntSet datastructure is a set datastructure with only
+    Integers as elements
+    """
     def __init__(self, a):
+        """
+        InsSet Initializer takes an list datastructure as input
+        validates it and returns the dictionary of hashed elements
+        as keys and elements as values
+
+        @param a : list or tuple of integers. Can be empty. 
+        
+        @returns : None
+        """
         assert hasattr(a, '__iter__')
         for i in a: self._validate(i)
         self.set_elems = {hash(item):item for item in a}
     
     @staticmethod
     def _validate(x):
+        """
+        Static method to validate the input list or tuple 
+        datastructure. Raises TypeError if not integer type
+        
+        @param x : object to be checked for integer type.
+        @returns : None
+
+        """
         if not isinstance(x, int):
             raise TypeError("Only Integer Arrays are allowed")
 
     def __contains__(self,a):
+        """
+        Container protocol for IntSet. 
+        Returns True if object 'a' is in IntSet
+
+        @returns : True or False
+        """
         if a in self.set_elems: # Since hash(int a) is an a 
             return True
         else:
             return False
         
     def __iter__(self):
+        """
+        Iterable protocol
+        """
         for i in self.set_elems:
             yield i
 
     def __eq__(self, rhs):
+        """
+        Defines equality of IntSet objects
+        """
         if not isinstance(rhs, IntSet):
             raise TypeError("Cannot compare IntSet with an unknown object")
         return self.set_elems == rhs.set_elems
 
     def __repr__(self):
+        """
+        Representation of IntSet object
+        """
         return("IntSet with elements {}".format(set(self.set_elems)))
 
     def issubset(self,b):
+        """
+        Checks if IntSet 'b' is subset of 'self'.
+        Raises TypeError if type of given input is not IntSet.
+
+        @returns : True if subset
+                   False if not subset
+        """
         if not isinstance(b, IntSet):
             raise TypeError("Given input is not of IntSet type:")
         for i in b:
@@ -37,6 +105,16 @@ class IntSet:
 
 
 def is_subset(a, b):
+    """
+    Constructs IntSet objects of 'a' and 'b' list or tuple objects
+    and tests is 'b' is a subset of 'a'
+
+    @param a: Potential superset of 'b'
+    @param b: Potential subset of 'a'
+
+    @returns : True - if 'b' is subset of 'a'
+               False - otherwise
+    """
     try:
         a = IntSet(a)
         b = IntSet(b)
--- Python/interview/posts_json.py
+++ Python/interview/posts_json.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python2
+
+# Copyright (C) 2016  <arhik@groundsignal.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import pprint
 import json
 import os
@@ -7,8 +25,12 @@ path = 'posts.json' #Path of the posts.json file
 preprocessed_path = 'post_posts.json'
 
 class jsonReader:
-    """Custom JSON Reader
-    ///Over Kill
+    """
+    Custom JSON Reader Context Manager
+    Gracefully exits and prompts the error on the console.
+    
+    @param path: path to the json file
+    @returns _file: file object
     """
     def __init__(self,path):
         self._path = path
@@ -27,31 +49,59 @@ class jsonReader:
         del self._file
 
 
-def preprocess_json():
+def preprocess_json(mandatory_keys=None):
+    """
+    Preprocess json file
+
+    @param mandatory_keys: keys which should present in the json.
+    @returns list object of parsed json file
+    """
     with jsonReader(path) as f:
         if f is not None:
             a = json.load(f, 'utf-8')
             for i in a:
-                if i.has_key(u'user'): # Preprocessing in case of missing keys like 'username', 'latitude'
-                    continue
-                else:
-                    a.pop(a.index(i))
+                for k in mandatory_keys:
+                    if i.has_key(k): # Preprocessing in case of missing keys like 'username', 'latitude'
+                        continue
+                    else:
+                        a.pop(a.index(i))
             return(a)
         return([])
 
-def read_posts():
-    return(preprocess_json())
+def read_posts(keys=None):
+    """
+    Read the posts
+    
+    @returns list object of posts
+    """
+    return(preprocess_json(mandatory_keys=keys))
 
 def getFollowersCount(post):
+    """
+    Utility function to get the followers count of a user
+
+    @returns int object
+    """
     return post[u'user'][u'followers']
 
 def geodesic_dist(a):
+    """
+    Computes the radial distance of the user from office using location information
+
+    @param a : dictionary of latitude and longitude
+    @returns float : distance of user from office
+    """
     return (math.acos(math.sin(math.radians(a[u'latitude']))*math.sin(math.radians(42.356680)) 
         + math.cos(math.radians(a[u'latitude']))*math.cos(math.radians(42.35668))*
         math.cos(math.radians(a[u'longitude']+71.060395))))
 
 def run():
-    posts = read_posts()
+    """
+    Function to find the user closest to the office
+
+    @returns dict - user information
+    """
+    posts = read_posts(keys=[u'user',u'location'])
     if len(posts)==0:
         return
 
@@ -63,10 +113,11 @@ def run():
                 posts[0][u'user'][u'followers'])) 
     
     # Finding the user closer to the office
+
     posts.sort(key=lambda x: geodesic_dist(x[u'location']))
     print("The user closer to our office is {0} with id-{1} .".format(
         posts[0][u'user'][u'username'], posts[0][u'id']))
-
+    return posts[0][u'user']
     
 if __name__=='__main__':
     run()
